000cee14641e845a50b456686df7a8d87230444b
Kou <hikkappi@yahoo.co.jp>
2023-05-31 00:15:08 +0900

    Seederを30件に増加。

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,31 +3,21 @@ import { PrismaClient, ParticipantStatus } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const participant1 = await prisma.participant.create({
-    data: {
-      name: 'Participant 1',
-      email: 'participant1@example.com',
-      status: ParticipantStatus.participating,
-    },
-  })
+  // 30人分のダミーデータを作成
+  const participants = Array.from({ length: 30 }).map((_, i) => ({
+    name: `Participant ${i + 1}`,
+    email: `participant${i}` + '@example.com',
+    status: ParticipantStatus.participating,
+  }))
 
-  const participant2 = await prisma.participant.create({
-    data: {
-      name: 'Participant 2',
-      email: 'participant2@example.com',
-      status: ParticipantStatus.adjourning,
-    },
-  })
-
-  const participant3 = await prisma.participant.create({
-    data: {
-      name: 'Participant 3',
-      email: 'participant3@example.com',
-      status: ParticipantStatus.withdrawn,
-    },
-  })
-
-  console.log({ participant1, participant2, participant3 })
+  // 非同期対応
+  await Promise.all(
+    participants.map(async (participant) => {
+      await prisma.participant.create({
+        data: participant,
+      })
+    }),
+  )
 }
 
 main()
