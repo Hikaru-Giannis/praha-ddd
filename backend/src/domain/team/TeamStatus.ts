1c3c8b07214e9798466a819b8d21259486cd2b96
Kou <hikkappi@yahoo.co.jp>
2023-06-13 23:33:47 +0900

    チーム名を値オブジェクト化。

--- backend/src/domain/team/TeamStatus.ts
+++ backend/src/domain/team/TeamStatus.ts
@@ -1,22 +1,34 @@
-// チームステータスの値オブジェクト
-type TeamStatusType = 'active' | 'inactive' | 'disbanded'
+const TEAM_STATUS = {
+  ACTIVE: 'active',
+  INACTIVE: 'inactive',
+  DISBANDED: 'disbanded',
+} as const
+type TeamStatusType = typeof TEAM_STATUS[keyof typeof TEAM_STATUS]
 
 export class TeamStatus {
   private constructor(public readonly value: TeamStatusType) {}
 
   public static active(): TeamStatus {
-    return new TeamStatus('active')
+    return new TeamStatus(TEAM_STATUS.ACTIVE)
   }
 
   public static inactive(): TeamStatus {
-    return new TeamStatus('inactive')
+    return new TeamStatus(TEAM_STATUS.INACTIVE)
   }
 
   public static disbanded(): TeamStatus {
-    return new TeamStatus('disbanded')
+    return new TeamStatus(TEAM_STATUS.DISBANDED)
+  }
+
+  public isActive(): boolean {
+    return this.value === TEAM_STATUS.ACTIVE
   }
 
   public isInactive(): boolean {
-    return this.value === 'inactive'
+    return this.value === TEAM_STATUS.INACTIVE
+  }
+
+  public isDisbanded(): boolean {
+    return this.value === TEAM_STATUS.DISBANDED
   }
 }
--- backend/src/domain/team/team.ts
+++ backend/src/domain/team/team.ts
@@ -1,6 +1,7 @@
 import { createRandomIdString } from 'src/util/random'
 import { TeamMember } from './team-member'
 import { TeamStatus } from './TeamStatus'
+import { TeamName } from './TeamName'
 
 type createTeamProps = {
   teamName: string
@@ -9,24 +10,16 @@ type createTeamProps = {
 
 export class Team {
   public readonly id: string
-  private readonly teamName: string
+  private readonly teamName: TeamName
   private readonly status: TeamStatus
   private readonly teamMembers: TeamMember[]
   private constructor(
     id: string,
-    teamName: string,
+    teamName: TeamName,
     status: TeamStatus,
     teamMembers: TeamMember[],
   ) {
     this.id = id
-    // 名前は数字のみ
-    if (!teamName.match(/^[0-9]+$/)) {
-      throw new Error('名前は数字のみです')
-    }
-    // 名前は3文字以下
-    if (teamName.length > 3) {
-      throw new Error('名前は3文字以下です')
-    }
     this.teamName = teamName
     this.status = status
     this.teamMembers = teamMembers
@@ -35,7 +28,7 @@ export class Team {
   static create({ teamName, teamMembers }: createTeamProps) {
     return new Team(
       createRandomIdString(),
-      teamName,
+      TeamName.create(teamName),
       TeamStatus.active(),
       teamMembers,
     )
@@ -47,7 +40,7 @@ export class Team {
 
   public assignTeamMember(teamMember: TeamMember): Team {
     return Team.create({
-      teamName: this.teamName,
+      teamName: this.teamName.value,
       teamMembers: [...this.teamMembers, teamMember],
     })
   }
