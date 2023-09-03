ebcb87d920593bc281b73c3033adb78c72ceaf9f
Kou <hikkappi@yahoo.co.jp>
2023-09-03 18:01:15 +0900

    メールにユニークチェックサービスのテストをリファクタリング。

--- backend/src/domain/participant/__tests__/validate-email-uniqueness-service.test.ts
+++ backend/src/domain/participant/__tests__/validate-email-uniqueness-service.test.ts
@@ -1,40 +1,51 @@
 import { ValidateEmailUniquenessService } from '../validate-email-uniqueness.service'
-import { IParticipantRepository } from '../participant.repository'
 import { Participant } from '../participant'
+import { Test, TestingModule } from '@nestjs/testing'
+import { tokens } from 'src/tokens'
+import { ParticipantInMemoryRepository } from 'src/infra/db/repository/participant/participant.in-memory.repository'
 
 describe('ValidateEmailUniquenessService', () => {
-  let validateEmailUniquenessService: ValidateEmailUniquenessService
-  let participantRepository: IParticipantRepository
+  let testApp: TestingModule
+  let participant: Participant
 
-  const participant = Participant.create({
-    name: 'test',
-    email: 'participant@example.com',
-  })
+  beforeEach(async () => {
+    participant = Participant.create({
+      name: 'test test',
+      email: 'test@example.com',
+    })
 
-  beforeEach(() => {
-    participantRepository = {
-      fetchAll: jest.fn(),
-      findById: jest.fn(),
-      findByEmail: jest.fn(),
-      save: jest.fn(),
-    }
-
-    validateEmailUniquenessService = new ValidateEmailUniquenessService(
-      participantRepository,
-    )
+    testApp = await Test.createTestingModule({
+      providers: [
+        {
+          provide: tokens.IParticipantRepository,
+          useClass: ParticipantInMemoryRepository,
+        },
+        {
+          provide: tokens.ValidateEmailUniquenessService,
+          useClass: ValidateEmailUniquenessService,
+        },
+      ],
+    }).compile()
   })
 
   it('参加者が存在しない場合、trueを返す', async () => {
-    jest.spyOn(participantRepository, 'findByEmail').mockResolvedValue(null)
+    const validateEmailUniquenessService = testApp.get(
+      tokens.ValidateEmailUniquenessService,
+    )
+    const participantRepository = testApp.get(tokens.IParticipantRepository)
+    participantRepository.items = []
+
     expect(await validateEmailUniquenessService.isUnique(participant)).toBe(
       true,
     )
   })
 
   it('参加者が存在する場合、falseを返す', async () => {
-    jest
-      .spyOn(participantRepository, 'findByEmail')
-      .mockResolvedValue(participant)
+    const validateEmailUniquenessService = testApp.get(
+      tokens.ValidateEmailUniquenessService,
+    )
+    const participantRepository = testApp.get(tokens.IParticipantRepository)
+    participantRepository.items = [participant]
 
     expect(await validateEmailUniquenessService.isUnique(participant)).toBe(
       false,
